f7ab4534456470cddffd39a60aec036f805b9f15
Angie Byron <webchick@webchick.net>
2025-01-07 06:59:14 -0800

    Now grab only the main header

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 // main.js
 import { gotScraping } from 'got-scraping';
+import * as cheerio from 'cheerio';
 
 const storeUrl = 'https://warehouse-theme-metal.myshopify.com/collections/sales';
 
+// Download HTML with Got Scraping
 const response = await gotScraping(storeUrl);
 const html = response.body;
-console.log(html);
+
+// Parse HTML with Cheerio
+const $ = cheerio.load(html);
+const headingElement = $('h1');
+const headingText = headingElement.text();
+
+// Print page title to terminal
+console.log(headingText);
